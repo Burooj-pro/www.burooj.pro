9e78bd2b5e93c7b553ad84ae21a08aa27678103c
Mutlaq Aldhbuiub <matulkn@gmail.com>
2022-06-23 09:44:49 +0300

    Update contact form

---
+++ components/sections/contact.vue
@@ -0,0 +1,25 @@
+<template>
+    <!-- Contact Form -->
+    <section id="contact" class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="contact-img col-12 col-md-5 d-flex align-items-center justify-content-end">
+                    <img src="~/assets/images/9.webp" class="rounded shadow-sm" alt="">
+                </div>
+                <div class="contact-form col-12 col-md-7 bg-white shadow-sm rounded-3 py-5 pe-4">
+                    <div class="pipedriveWebForms" data-pd-webforms="https://webforms.pipedrive.com/f/6W8B7uZHws18uQtXpbXhoS7DhXv8sUTQlw9zzy8SYAESjOJ1MNxMfXt9X4ZqryjL2j"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
